b185b6095be1c02dd6ffd6b76a3212b4aac23b26
krishnadas <krishnadas@localhost>
2022-06-20 16:53:25 -0400

    change

---
+++ exam/src/test/Q4.java
@@ -0,0 +1,25 @@
+package test;
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		String output =returnSomething("cow");
+		System.out.println(output);
+
+	}
+	public static String returnSomething(String s) {
+		if(s == "cow") {
+			s= "Animal";
+		}else if(s == "table" ) {
+			s= "objcets";
+		
+			
+		}else  {
+			s= "nothing";
+			
+			//System.out.println(s);
+		}
+		return s+"hi";
+	}	
+
+}
